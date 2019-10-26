dfa2f48c5f2960b3148a3d309cc0cf9655dd3cbf
Victor <48688205+Sennesiel@users.noreply.github.com>
2019-10-26 19:59:33 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+typedef struct ?????
+{
+	bool operator< (const ????? & other) const
+	{
+		return ???? < other.???; // signe inversé pour une pq;
+	}
+}?????;
+*/
+
+//const int ????? = ;
+
+#define fori(a,b) for(int a = 0; a < b; a++)
+#define pri(a) printf("%d ", a)
+#define prnl() printf("\n")
+#define si(a) scanf("%d", &a)
+#define snl() scanf("\n")
+#define min3(a,b,c) min(a,min(b,c))
+#define vi vector<int>
+#define vb vector<bool>
+#define qi queue<int>
+
+void init()
+{
+
+}
+
+int main()
+{
+
+}
